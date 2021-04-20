599daf67e761220cc22c514dea1cddaa57de978c
Rohit <rohit5012000@gmail.com>
2021-04-20 08:27:05 +0530

    nothing

--- src/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export async function getUsers(page) {
-  const users = await axios.get(
-    `https://api.stackexchange.com/2.2/search/advanced?page=${page}&order=desc&sort=activity&site=stackoverflow`
-  );
-  return users.data.items;
-}
